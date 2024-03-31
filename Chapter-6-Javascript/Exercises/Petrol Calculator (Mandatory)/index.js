0e336f2af14a1a62317a75023aba72ae2fe6a135
Mohamed Shakin <m.shakin2005@gmail.com>
2024-03-31 21:34:32 +0400

    Update index.js

--- Chapter-6-Javascript/Exercises/Petrol Calculator (Mandatory)/index.js
+++ Chapter-6-Javascript/Exercises/Petrol Calculator (Mandatory)/index.js
@@ -2,6 +2,6 @@ function calculate() {
     var price_per_litre = document.getElementById('Petrol_Price').value;
     var litres = document.getElementById('liters').value;
     var total_price = price_per_litre * litres
-    document.getElementById('total_Amount').innerHTML = total_price;
+    document.getElementById('total_Amount').innerHTML = "AED " + total_price;
 
 }
