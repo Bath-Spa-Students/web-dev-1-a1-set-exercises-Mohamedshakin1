f39aae866c6eea7fe59784e8f7cd728b63b6025c
Mohamed Shakin <m.shakin2005@gmail.com>
2024-04-02 22:15:45 +0400

    Update index.js

--- Chapter-5-Embedded Content/Exercises/Soundboard (Bonus Part-2)/index.js
+++ Chapter-5-Embedded Content/Exercises/Soundboard (Bonus Part-2)/index.js
@@ -43,3 +43,16 @@ function mystery() {
     var audio=document.getElementById("sound9")
     sound9.play();
 }
+
+function textToAudio () {
+    let msg= document.getElementById("text-to-speech").value;
+    let speech= new SpeechSynthesisUtterance();
+    speech.lang= "en-US";
+    speech.text= msg;
+    speech.volume=1;
+    speech.rate=1;
+    speech.pitch=1;
+    window.speechSynthesis.speak(speech);
+
+
+}
