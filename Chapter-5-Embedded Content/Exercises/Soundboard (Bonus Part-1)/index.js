25ab58dff1d79109c247044647a0b1c5c5b9cb54
Mohamed Shakin <m.shakin2005@gmail.com>
2024-04-02 19:10:09 +0400

    Soundboard (Bonus Part 1)

---
+++ Chapter-5-Embedded Content/Exercises/Soundboard (Bonus Part-1)/index.js
@@ -0,0 +1,48 @@
+function aha () {
+    var audio=document.getElementById("sound1")
+    sound1.play();
+}
+
+function dan () {
+    var audio=document.getElementById("sound2")
+    sound2.play();
+}
+
+function back() {
+    var audio=document.getElementById("sound3")
+    sound3.play();
+}
+
+function bang() {
+    var audio=document.getElementById("sound4")
+    sound4.play();
+}
+
+function email() {
+    var audio=document.getElementById("sound5")
+    sound5.play();
+}
+
+
+function egg() {
+    var audio=document.getElementById("sound6")
+    sound6.play();
+}
+
+function confused() {
+    var audio=document.getElementById("sound7")
+    sound7.play();
+}
+
+function hello() {
+    var audio=document.getElementById("sound8")
+    sound8.play();
+}
+
+function mystery() {
+    var audio=document.getElementById("sound9")
+    sound9.play();
+}
+
+
+
