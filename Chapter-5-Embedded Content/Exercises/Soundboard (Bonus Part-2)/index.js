48ee249cac20780047200f46ff6ca3cc0a3c1e62
Mohamed Shakin <m.shakin2005@gmail.com>
2024-04-06 21:14:29 +0400

    Update index.js

--- Chapter-5-Embedded Content/Exercises/Soundboard (Bonus Part-2)/index.js
+++ Chapter-5-Embedded Content/Exercises/Soundboard (Bonus Part-2)/index.js
@@ -1,54 +1,63 @@
+//Created a function named aha()
 function aha () {
-    var audio=document.getElementById("sound1")
-    sound1.play();
+    var audio=document.getElementById("sound1")  //audio of sound1 will be retrieve by getting their respective Id and will be stored variable audio
+    sound1.play();  //It will play audio of sound1 once the button is clicked
 }
 
+//Created a function named dan()
 function dan () {
-    var audio=document.getElementById("sound2")
-    sound2.play();
+    var audio=document.getElementById("sound2") //audio of sound2 will be retrieve by getting their respective Id and will be stored variable audio
+    sound2.play();  //It will play audio of sound2 once the button is clicked
 }
 
+//Created a function named back()
 function back() {
-    var audio=document.getElementById("sound3")
-    sound3.play();
+    var audio=document.getElementById("sound3") //audio of sound3 will be retrieve by getting their respective Id and will be stored variable audio
+    sound3.play();   //It will play audio of sound3 once the button is clicked
 }
 
+//Created a function named bang()
 function bang() {
-    var audio=document.getElementById("sound4")
-    sound4.play();
+    var audio=document.getElementById("sound4") //audio of sound4 will be retrieve by getting their respective Id and will be stored variable audio
+    sound4.play();  //It will play audio of sound4 once the button is clicked
 }
 
+//Created a function named email()
 function email() {
-    var audio=document.getElementById("sound5")
-    sound5.play();
+    var audio=document.getElementById("sound5") //audio of sound5 will be retrieve by getting their respective Id and will be stored variable audio
+    sound5.play();  //It will play audio of sound5 once the button is clicked
 }
 
-
+//Created a function named egg()
 function egg() {
-    var audio=document.getElementById("sound6")
-    sound6.play();
+    var audio=document.getElementById("sound6") //audio of sound6 will be retrieve by getting their respective Id and will be stored variable audio
+    sound6.play();  //It will play audio of sound6 once the button is clicked
 }
 
+//Created a function named confused()
 function confused() {
-    var audio=document.getElementById("sound7")
-    sound7.play();
+    var audio=document.getElementById("sound7") //audio of sound7 will be retrieve by getting their respective Id and will be stored variable audio
+    sound7.play();  //It will play audio of sound7 once the button is clicked
 }
 
+//Created a function named hello()
 function hello() {
-    var audio=document.getElementById("sound8")
-    sound8.play();
+    var audio=document.getElementById("sound8") //audio of sound8 will be retrieve by getting their respective Id and will be stored variable audio
+    sound8.play();   //It will play audio of sound8 once the button is clicked
 }
 
+//Created a function named mystery()
 function mystery() {
-    var audio=document.getElementById("sound9")
-    sound9.play();
+    var audio=document.getElementById("sound9") //audio of sound9 will be retrieve by getting their respective Id and will be stored variable audio
+    sound9.play();   //It will play audio of sound9 once the button is clicked
 }
 
+//Created a function named textToAudio()
 function textToAudio () {
-    let msg= document.getElementById("text-to-speech").value;
-    let speech= new SpeechSynthesisUtterance();
-    speech.lang= "en-US";
-    speech.text= msg;
+    let msg= document.getElementById("text-to-speech").value; //value will be retrieve by getting their respective Id and will be stored variable msg
+    let speech= new SpeechSynthesisUtterance();  // SpeechSynthesisUtterance() is a function, it converts text into epeech 
+    speech.lang= "en-US";  //language is English
+    speech.text= msg;      // it converts text written in msg variable to speech
     speech.volume=1;
     speech.rate=1;
     speech.pitch=1;
