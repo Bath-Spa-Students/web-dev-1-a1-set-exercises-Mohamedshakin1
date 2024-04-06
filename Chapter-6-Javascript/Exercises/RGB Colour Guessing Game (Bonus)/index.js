e19dc71c8cb94980da8b8504ac63e1e195802905
Mohamed Shakin <m.shakin2005@gmail.com>
2024-04-06 21:33:02 +0400

    Update index.js

--- Chapter-6-Javascript/Exercises/RGB Colour Guessing Game (Bonus)/index.js
+++ Chapter-6-Javascript/Exercises/RGB Colour Guessing Game (Bonus)/index.js
@@ -7,7 +7,8 @@ var b=document.getElementById('second')
 var c=document.getElementById('third')
 var result= document.getElementById('result')
 
-
+//Created a function named random()
+//Each time function random is called, each time it will generate different random colors
 function randomColor() {
     var r= Math.floor(Math.random()*256)
     var g= Math.floor(Math.random()*256)
@@ -15,22 +16,23 @@ function randomColor() {
     return "rgb(" + r +", " + g +", " + b +")";
 }
 
-
+//Created a function fillColor()
 function fillColor() {
-        var x=box1.style.backgroundColor= randomColor()
-        a.innerHTML=x
-        var y=box2.style.backgroundColor= randomColor()
-        b.innerHTML=y
-        var z=box3.style.backgroundColor= randomColor()
-        c.innerHTML=z
+        var x=box1.style.backgroundColor= randomColor()  //it will fill random color to box1
+        a.innerHTML=x  //it will insert value of first box color
+        var y=box2.style.backgroundColor= randomColor()  //it will fill random color to box2
+        b.innerHTML=y  //it will insert value of second box color
+        var z=box3.style.backgroundColor= randomColor()  //it will fill random color to box3
+        c.innerHTML=z  //it will insert value of third box color
         var colors=[x,y,z]
-        var pickColor= Math.floor(Math.random()* colors.length);
-        color_name.innerHTML= colors[pickColor]
+        var pickColor= Math.floor(Math.random()* colors.length);  //it will pick any color among three random colors
+        color_name.innerHTML= colors[pickColor]    // picked color will be inserted to Id selector of 'rgb-name'
 
 }
 
-
+//Created a function fillColor()
 function colorResult() {
+    // Whichever box color is matched to picked color will be display as the correct answer in Id selector of 'result' after clicking any of three boxes
     if (color_name.textContent==a.textContent){
         result.innerHTML="A is the right answer"
     }
@@ -42,7 +44,17 @@ function colorResult() {
     }  
 }
 
+//Created a function fillColor()
+//To clear the result for next round, we use just ""
+function clear() {
+    result.innerHTML=""
+}
+
+//Created a function game()
+//Inside function game(), it contains three function that is randomColor(), fillColor, space
+//This function used for to play next round of the game
 function game() {
-    randomColor();
+    randomColor();  
     fillColor();
+    space()
 }
